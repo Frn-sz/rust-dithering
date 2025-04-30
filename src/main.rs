015c6bf60a4274990ab9bf5d30ce2d2d38487272
Frn-sz <fernandogdemedeirosw@gmail.com>
2025-04-29 23:24:43 -0300

    docs: add comments

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use clap::Parser;
 use image::{DynamicImage, GenericImageView, ImageReader};
 use std::path::PathBuf;
 use std::time::Instant;
-use utils::{quantify, saturating_add, save_as_rgb};
+use utils::{quantize, saturating_add, save_as_rgb};
 
 //Struct de argumentos que serão parseados pela clap
 #[derive(Parser, Debug)]
@@ -13,12 +13,15 @@ struct Args {
     #[arg(short, long)]
     image: PathBuf,
 
+    //Caminho para salvar o arquivo de saída
     #[arg(short, long)]
     save: PathBuf,
+
     // Boolean para indicar se o programa deve ou não converter a imagem para escala de cinza
     #[arg(short, long, default_value_t = false)]
     gray: bool,
 
+    //Tamanho da paleta de cores. Por padrão 2 (0,255). Quanto maior a paleta, menor o gap entre cada cor (e.g 3 = [0,127,255])
     #[arg(short, long, default_value_t = 2)]
     palette: usize,
 }
@@ -36,11 +39,12 @@ fn main() -> Result<(), anyhow::Error> {
     //Gerando a paleta de cores de acordo com o parâmetro da CLI
     let palette = gen_palette(args.palette);
 
-    //Verificando se precisamos aplicar algoritmo com cores ou para escala de cinza
+    //Verifica se é necessário converter a imagem para cinza
     if args.gray {
         img = img.grayscale();
     }
 
+    //Aplica algoritmo de ditherização
     dither(img, &args.save, palette);
 
     // Calcula o tempo decorrido
@@ -48,22 +52,25 @@ fn main() -> Result<(), anyhow::Error> {
 
     // Imprime o tempo em milissegundos
     println!("Tempo de execução: {} ms", duration.as_millis());
+
     Ok(())
 }
 
 fn gen_palette(size: usize) -> Vec<u8> {
-    if size == 0 {
-        return vec![];
-    }
-    if size == 1 {
-        return vec![0];
+    //Se o tamanho for 1,0 ou negativo, usa por padrão 2 cores por canal
+    if size <= 1 {
+        return vec![0, 255];
     }
 
+    //Aloca um vetor com o tamanho desejado de paleta
     let mut palette = Vec::with_capacity(size);
+
+    //Gera a paleta de cores de acordo com o tamanho passado
     for i in 0..size {
         let value = ((i as u32 * 255) / (size as u32 - 1)) as u8;
         palette.push(value);
     }
+
     palette
 }
 
@@ -81,18 +88,23 @@ fn dither(img: DynamicImage, path_to_save: &PathBuf, palette: Vec<u8>) {
             //Pegar pixel atual (posição (x,y))
             let pixel = img.get_pixel(x, y);
 
+            //Pega o valor de cada canal do pixel
             let [r, g, b, _] = pixel.0;
 
+            //Coloca o valor de cada canal nas matrizes de canais
             r_channel[y as usize][x as usize] = r;
             g_channel[y as usize][x as usize] = g;
             b_channel[y as usize][x as usize] = b;
         }
     }
 
+    //Executa algoritmo de Floyd Steinberg
     floyd_steinberg(
         &mut r_channel,
         &mut g_channel,
         &mut b_channel,
+        height as usize,
+        width as usize,
         path_to_save,
         palette,
     );
@@ -102,23 +114,22 @@ fn floyd_steinberg(
     r: &mut Vec<Vec<u8>>,
     g: &mut Vec<Vec<u8>>,
     b: &mut Vec<Vec<u8>>,
+    height: usize,
+    width: usize,
     path_to_save: &PathBuf,
     palette: Vec<u8>,
 ) {
-    //Pegando dimensões da imagem
-    let height = r.len();
-    let width = r[0].len();
-
     //Percorrendo a matriz dos canais para aplicar o algoritmo
     for y in 0..height {
         for x in 0..width {
             //Quantizando canais e pegando erro de cada pixel
-            let error_r = quantify(r, x, y, &palette);
-            let error_g = quantify(g, x, y, &palette);
-            let error_b = quantify(b, x, y, &palette);
+            let error_r = quantize(r, x, y, &palette);
+            let error_g = quantize(g, x, y, &palette);
+            let error_b = quantize(b, x, y, &palette);
 
             // Espalhar erro para os vizinhos
             if x + 1 < width {
+                //Soma 7/16 do erro no pixel à direita
                 r[y][x + 1] = saturating_add(r[y][x + 1], (error_r * 7 / 16) as i8);
                 g[y][x + 1] = saturating_add(g[y][x + 1], (error_g * 7 / 16) as i8);
                 b[y][x + 1] = saturating_add(b[y][x + 1], (error_b * 7 / 16) as i8);
@@ -126,15 +137,18 @@ fn floyd_steinberg(
 
             if y + 1 < height {
                 if x > 0 {
+                    //Soma 3/16 do erro no pixel à diagonal inferior esquerda
                     r[y + 1][x - 1] = saturating_add(r[y + 1][x - 1], (error_r * 3 / 16) as i8);
                     g[y + 1][x - 1] = saturating_add(g[y + 1][x - 1], (error_g * 3 / 16) as i8);
                     b[y + 1][x - 1] = saturating_add(b[y + 1][x - 1], (error_b * 3 / 16) as i8);
                 }
+                //Soma 5/16 do erro no pixel abaixo
                 r[y + 1][x] = saturating_add(r[y + 1][x], (error_r * 5 / 16) as i8);
                 g[y + 1][x] = saturating_add(g[y + 1][x], (error_g * 5 / 16) as i8);
                 b[y + 1][x] = saturating_add(b[y + 1][x], (error_b * 5 / 16) as i8);
 
                 if x + 1 < width {
+                    //Soma 1/16 do erro no pixel à diagonal inferior direita
                     r[y + 1][x + 1] = saturating_add(r[y + 1][x + 1], (error_r * 1 / 16) as i8);
                     g[y + 1][x + 1] = saturating_add(g[y + 1][x + 1], (error_g * 1 / 16) as i8);
                     b[y + 1][x + 1] = saturating_add(b[y + 1][x + 1], (error_b * 1 / 16) as i8);
@@ -143,5 +157,6 @@ fn floyd_steinberg(
         }
     }
 
-    save_as_rgb(r, g, b, path_to_save);
+    //Salva a imagem de saída
+    save_as_rgb(r, g, b, height, width, path_to_save);
 }
--- src/utils.rs
+++ src/utils.rs
@@ -2,34 +2,50 @@ use std::path::PathBuf;
 
 use image::{Rgb, RgbImage};
 
-pub fn quantify(channel: &mut Vec<Vec<u8>>, x: usize, y: usize, palette: &Vec<u8>) -> i16 {
+pub fn quantize(channel: &mut Vec<Vec<u8>>, x: usize, y: usize, palette: &Vec<u8>) -> i16 {
+    //Pega o valor da cor original no pixel (x,y)
     let old = channel[y][x] as i16;
+    //Acha a cor mais próxima na paleta
     let new = find_closest_color(palette, old as u8) as i16;
+
+    //Altera o valor no canal para a nova cor
     channel[y][x] = new as u8;
 
+    //Retorna o erro
     old - new
 }
 
 pub fn saturating_add(value: u8, increment: i8) -> u8 {
+    // Verifica se o incremento é negativo
     if increment.is_negative() {
+        /*  Se for negativo, converte para positivo com wrapping_abs
+        e subtrai do valor original usando saturating_sub. Se o resultado for menor que 0, fica truncado em 0. */
         value.saturating_sub(increment.wrapping_abs() as u8)
     } else {
+        // Se for positivo, converte para u8 e usa saturating_add. Se for maior que 255, fica truncado em 255.
         value.saturating_add(increment as u8)
     }
 }
 
-pub fn save_as_rgb(r: &Vec<Vec<u8>>, g: &Vec<Vec<u8>>, b: &Vec<Vec<u8>>, path: &PathBuf) {
-    let height = r.len();
-    let width = r[0].len();
-
+pub fn save_as_rgb(
+    r: &Vec<Vec<u8>>,
+    g: &Vec<Vec<u8>>,
+    b: &Vec<Vec<u8>>,
+    height: usize,
+    width: usize,
+    path: &PathBuf,
+) {
+    //Cria imagem nova com mesma largura e altura
     let mut img = RgbImage::new(width as u32, height as u32);
 
+    //Percorre os canais colocando os valores nos pixels da imagem recém criada
     for y in 0..height {
         for x in 0..width {
             img.put_pixel(x as u32, y as u32, Rgb([r[y][x], g[y][x], b[y][x]]));
         }
     }
 
+    //Salva imagem no caminho de saída
     img.save(path).expect("Erro ao salvar imagem");
 }
 fn find_closest_color(palette: &Vec<u8>, value: u8) -> u8 {
@@ -37,9 +53,13 @@ fn find_closest_color(palette: &Vec<u8>, value: u8) -> u8 {
         return 0;
     }
 
+    //Começa do início da paleta
     let mut closest = palette[0];
+    /* Inicializa a menor diferença como sendo a diferença entre o pixel e o valor
+    da primeira cor da paleta */
     let mut smallest_diff = (value as i16 - closest as i16).abs();
 
+    //Percorre a paleta procurando a cor mais próxima do valor do pixel
     for &color in palette {
         let diff = (value as i16 - color as i16).abs();
         if diff < smallest_diff {
